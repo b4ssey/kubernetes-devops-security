802d0325fe4003421d1191d875f00c1d7e0f0c2d
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-09-30 11:07:45 +0100

    checking logs for advice

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -6,7 +6,7 @@
 scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
 scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r ) 
 scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score ) 
-
+scan_advice=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].advice ) 
 
 # using kubesec docker image for scanning
 # scan_result=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml)
@@ -22,6 +22,7 @@ scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https:/
 	    echo "Kubesec Scan $scan_message"
 	else
 	    echo "Score is $scan_score, which is less than or equal to 5."
+        echo "What to do. $scan_advice"
 	    echo "Scanning Kubernetes Resource has Failed"
 	    exit 1;
 	fi;
